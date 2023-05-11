0dbb6e8f092e123b10737c4098a2cab7abb6a695
SMAILe3001 <smaile3001@gmail.com>
2023-05-11 21:31:53 +0300

    fix style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ export const App = () => {
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
